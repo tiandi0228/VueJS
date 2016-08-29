5f948fb0ee591f9670a4a5ae51c4f427766f9d0e
tiandi0228 <tiandi0228@live.com>
2016-08-29 20:58:34 +0800

    登录页面

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,20 +1,28 @@
 <template>
-	<div class="login">
+	<div class="loginBox">
 		<input type="" name="">
 		<button>登录</button>
 	</div>
 </template>
 
 <style>
-	.login{
+	.loginBox{
 		margin: 30px 0 0 0;
 	}
-	.login input{
+	.loginBox input{
 		width: 100%;
 		height: 40px;
 		padding: 0 10px;
 	}
-	.login button{
-		
+	.loginBox button{
+		font-size: 16px;
+		margin-top: 20px;
+		width: 100%;
+		height: 36px;
+		border: none;
+		background-color: #80bd01;
+		color: #fff;
+		font-weight: bold;
+		letter-spacing: 8px;
 	}
 </style>
